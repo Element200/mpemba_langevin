d7769dbddb60d7b318970420a3488d9d8cbe8c77
Element200 <38728443+Element200@users.noreply.github.com>
2025-07-21 15:42:32 -0700

    Allow for more options when extracting file data
    extract_file_data can now output the results of diagnostic variables as well as just the position data, and chunk_splitter can also spit out the data from states other than the protocol state (provided the state is fixed-length)

--- file_processing.py
+++ file_processing.py
@@ -14,7 +14,7 @@ import pandas as pd
 from tqdm import tqdm
 import xarray as xr
 
-def chunk_splitter(dataframe, dt=1e-5):
+def chunk_splitter(dataframe, dt=1e-5, state_to_extract='protocol', state_phase_lag=0, states = {'calibration' : 0, 'positioning' : 1, 'protocol' : 2}):
     """
     Turn one giant dataarray of particle trajectories into an xarray DataArray that stores each individual particle's trajectory in a new dimension --- this gives us something we can work with.
 
@@ -22,6 +22,15 @@ def chunk_splitter(dataframe, dt=1e-5):
     ----------
     dataframe : pandas.DataFrame
         The raw data with column names set appropriately. This code will fail if certain column names do not exist.
+    state_to_extract : str, optional
+        state to extract. The code will throw away the other states to save memory.
+        If state_to_extract represents a state with non fixed-length data, this code will break.
+    state_phase_lag : int, optional
+        If the state resets at the moment the time variable is also reset to 0, this is equal to zero. If it lags by 1, set this to one, etc. The default is 0 (i.e. assuming that the state variable resets properly with time).
+    states : dict, optional
+        Dictionary with key-value pairs explaining state codes. state_to_extract must be one of the keys. This code will break if 
+            
+        
 
     Returns
     -------
@@ -31,17 +40,20 @@ def chunk_splitter(dataframe, dt=1e-5):
     """
     # Each chunk contains one particle's trajectory
     # To save memory, we will get rid of the time column (assuming all time columns are identical)
-    masked_data = dataframe[dataframe['state']==2]
+    masked_data = dataframe[dataframe['state']==states[state_to_extract]]
     forward = dataframe[1:].reset_index(drop=True)
-    pivots = dataframe[(forward['state']==0) & (dataframe['state']==2)].index + 1# This is a bit complex
+    pivots = dataframe[(forward['state']==states['calibration']) & (dataframe['state']==states['protocol'])].index + 1 - state_phase_lag # Assumes 'calibration' is the first state and 'protocol' is the last.
     chunks = []
     for i in range(len(pivots)-1):
         chunks.append(masked_data.loc[pivots[i]:pivots[i+1]])
     chunks = np.array(chunks)
-    times = chunks[0,:,1]*dt # Assumes all time columns are identical
-    return xr.DataArray(chunks[...,0], dims=['n','t'], coords={'t':times}) # Throw away the voltage and time data (time is redundant between all chunks) and also state data (because we filtered it so it's all =2)
+    data_cols = list(dataframe.columns)
+    cols_to_extract = {col: data_cols.index(col) for col in data_cols if (not col in ['t','state'])} # We extract all of the columns except for time and state
+    t_index = data_cols.index('t') # Get index of the time column
+    times = chunks[0,:,t_index]*dt # Assumes all time columns are identical
+    return xr.Dataset(data_vars={col: (['n','t'],chunks[...,cols_to_extract[col]]) for col in cols_to_extract.keys()}, coords={'t':times}) # Throw away the voltage and time data (time is redundant between all chunks) and also state data (because we filtered it so it's all =2)
 
-def extract_file_data(filenames, protocol_time, dt=1e-5, column_names=['x','t','V','state'], temperatures = ['h','w','c']):
+def extract_file_data(filenames, protocol_time, dt=1e-5, column_names=['x','t','drift','state','x0'], cols_to_extract= ['x'], temperatures = [1000,12,1]):
     """
     Convert experimental file data into an ensemble with the same structure as that of the simulation.
 
@@ -54,9 +66,9 @@ def extract_file_data(filenames, protocol_time, dt=1e-5, column_names=['x','t','
     dt : numeric, optional
         Timestep in seconds. Must be less than the length of the protocol The default is 1e-5.
     column_names : , list of str
-        Names of the columns in the output data. The default is ['x','t','V','state'].
-    temperatures : list of str, optional
-        Key names for the initial temperatures at which the protocol happens, eg. 'h', 'w', 'c' for 'hot','warm', 'cold' respectively. The default is ['h','w','c'].
+        Names of the columns in the output data. The default is ['x','t','drift','state', 'x0'].
+    temperatures : list of numerics, optional
+        The initial temperatures, sorted *in descending order*. The default is [1000,10,1].
 
     Returns
     -------
@@ -67,14 +79,19 @@ def extract_file_data(filenames, protocol_time, dt=1e-5, column_names=['x','t','
     chunks = {}
     n_min = np.inf # Minimum number of particles in an array. Initially set to infinity because that's much higher than any experiment will realistically produce
     for filename in filenames:
-        chunks[filename] = chunk_splitter(pd.read_table(filename, names=column_names, usecols=['x','t','state']))
-        if chunks[filename].shape[0] < n_min:
-            n_min = int(chunks[filename].shape[0]) # Once this for loop is done running, n_min will hold the lowest number of particles in the dataarray
-    data = np.zeros((len(filenames), n_min, int(protocol_time//dt)))
-    for i in range(len(filenames)):
-        filename = filenames[i]
-        data[i,...] = chunks[filename].loc[:n_min, ...] # This ensures that data will contain fixed-length data along the n dimension as well
-    array = xr.DataArray(data, dims=['T','n','t'], coords={'T':temperatures, 't': np.arange(0,protocol_time,dt)})
+        chunks[filename] = chunk_splitter(pd.read_table(filename, names=column_names, usecols=[*cols_to_extract,'t','state']))
+        n = int(chunks[filename]['n'][-1]) # Nymber of particles in chunks['filename']
+        if n < n_min:
+            n_min = int(chunks[filename]['n'][-1]) # Once this for loop is done running, n_min will hold the lowest number of particles in the dataarray
+    # data_array = {var: xr.zeros.zeros((len(filenames), n_min, int(protocol_time//dt))) for var in chunks[filenames[0]].data_vars}
+    # for i in range(len(filenames)):
+    #     filename = filenames[i]
+    #     for var in chunks[filename].data_vars:
+    #         data_array[var][i,...] = chunks[filename][var].loc[:n_min, ...] # This ensures that data will contain fixed-length data along the n dimension as well
+    array = []
+    for var in chunks[filenames[0]].data_vars:
+        array.append(xr.concat([chunks[filename][var].loc[:n_min,...].assign_coords(t=np.arange(0,protocol_time,dt)) for filename in filenames], pd.Index(temperatures, name='T')))
+    # array = xr.DataArray(data, dims=['T','n','t'], coords={'T':temperatures, 't': np.arange(0,protocol_time,dt)})
     return array
 
 def load_processed_data(filenames, temperatures=[1000,12,1]):
@@ -100,3 +117,7 @@ def load_processed_data(filenames, temperatures=[1000,12,1]):
         data.append(pd.read_csv(filenames[i], header = 0, index_col=0).T)
     t = data[-1].columns # Assumes the time columns are all the same
     return xr.DataArray(data, dims=('T', 'n', 't'), coords = {'T': temperatures, 't':t})
+
+if __name__ == '__main__':
+    filenames = [f"../Data/2025_06_30/data_{key}.txt" for key in ['h','w','c']]
+    data = extract_file_data(filenames, 7e-2, cols_to_extract=['x','drift'])
